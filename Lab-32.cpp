8dfbedde0214a1a904fd46fac6dea7f872ab45b2
Tracy-115 <kmin984@insite.4cd.edu>
2025-11-14 16:49:32 -0800

    added some examples for using deque

--- Lab-32.cpp
+++ Lab-32.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main(){
 
-    deque <car> Cque;
-    Cque.push_back(car());
-    Cque.push_back(car()); // added 2 cars to the back 
+    deque <Car> Cque;
+    Cque.push_back(Car());
+    Cque.push_back(Car()); // added 2 cars to the back 
+
+    cout << "Initially:" << endl;
+    for (auto&c : Cque){
+        Cque.print();
+    }
+
+
+    Cque.pop_back(); //removing the front
+    Cque.push_front(Car());
+    Cque.pop_front(); //removing the back
+    cout << Cque.size() << endl; //this will tell us how many objects are in there
+    Cque.front(); //accessing the front
+    Cque.back(); //accessing the back
+
 }
