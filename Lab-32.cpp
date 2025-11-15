1457598ea4c4df3fc8e5d4d3eb473eb08c430ed1
Tracy-115 <kmin984@insite.4cd.edu>
2025-11-14 17:05:54 -0800

    started mile 2

--- Lab-32.cpp
+++ Lab-32.cpp
@@ -3,8 +3,37 @@
 using namespace std;
 
 int main(){
-
+    const int max_size = 2; //this is to make sure I don't have over 2 objects
+    srand(time(0)); //so that it is random
     deque <Car> Cque;
+
+    for (int i = 0, i <max_size, ++i){
+        Cque.push_back(Car());
+    }
+    cout << "Initial Condition" << endl;
+    for(auto&c : Cque){
+        c.print;
+    }
+    while (!Cque.empty()){
+        int percent = rand() % 100;
+
+        if (percent <= 45){
+            Cque.push_back(Car());
+            cout << Cque.back().print() << "joined the que" << endl;
+        }
+        else {
+            cout << Cque.front().print() << ""
+            Cque.pop_front();
+        }
+    }
+
+
+
+
+
+
+    //Milestone 1
+    cout << "This is from milestone 1" << endl;
     Cque.push_back(Car());
     Cque.push_back(Car()); // added 2 cars to the back 
 
@@ -12,13 +41,11 @@ int main(){
     for (auto&c : Cque){
         c.print(); // the print() comed from Car.h file
     }
-
-
     Cque.pop_back(); //removing the front
     cout << "After popping:" << endl;
     for (auto&c : Cque){
         c.print();
-    }
+    } 
     Cque.push_front(Car());
     Cque.pop_front(); //removing the back
     cout << "Size: " << Cque.size() << endl; //this will tell us how many objects are in there
