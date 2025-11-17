990754c52f369c3e5061b34c533cd39b5011126b
Tracy-115 <kmin984@insite.4cd.edu>
2025-11-16 19:38:27 -0800

    better formatted, changed some names

--- Lab-32.cpp
+++ Lab-32.cpp
@@ -1,4 +1,4 @@
-#include "Car.h"//included the Car.h file
+#include "Cars.h"//included the Car.h file
 #include <deque> //added deque to my code
 #include <iostream>
 #include <ctime>
@@ -7,7 +7,7 @@ using namespace std;
 //Your code has created the array of deques, and tests operations on this complex data structure to validate it.
 
 int main(){
-    const int leave = 46, int join = 39, int shift = 15;
+    const int leave = 46, join = 39, shift = 15;
     srand(time(0)); //so that it is random
     const int lane = 4; //this is to make sure I don't have over 2 objects
     deque<Car> Cque[4];
@@ -27,55 +27,56 @@ int main(){
         cout << endl;
     }//because of the change in the first loop, this loop pack is going to print 2 cars for each lane
     for (int t=0; t < 20; t++){
-        cout << "Time: " << t << endl;
-    int n = 1;
-    for(int i =0; i < lane; i++){
+        cout << "Time: " << t+1 << endl;
+        int n = 1;
         int percent = rand() % 100;
-        cout << endl;
-        cout << "Operation " << n << endl;
-        if (percent < 50){ //changed to 50%
-            Cque[i].push_back(Car());
-            cout << "Car just joined: " ;
-            Cque[i].back().print();
-        }
-        else {
-            cout << "Just paid and left: "; //I added this before popping because after we pop, there will be a new car at the front 
-            Cque[i].front().print();
-            Cque[i].pop_front();
-        }
-        if (percent < leave){ //the paid and joined loops are pretty similar to the onees above
-            cout << "Paid: ";
-            Cque[i].front().print;
+        for(int i =0; i < lane; i++){
             cout << endl;
-            Cque[i].pop_front();
-        }
-        else if (percent < leave + join){
-            Cque.push_back();
-            cout << "Joined: ";
-            Cque.back().print();
-        }
-        else if (percent < leave + join + shift){
-            Car temp = Cque[i].back(); //what i did here was I stored one data into temp and then remove it from the initial lanne, and then add it into a new lane
-            Cque[i].pop_back();
-            int Lane;
-            Lane = rand()%lane;
-            Cque[Lane].push_back(temp);
-            cout << "Switched: ";
-            Cque[Lane].back().print();
-
-        }
-                if(Cque[i].empty()){
-            cout << "Queue now empty." << endl; //if the que is empty after going through if else statements, this will be printed
-        }
-        else {
-            cout << "Queue: " << endl;
-            for (auto&c : Cque[i]){
-            c.print(); // the print() comed from Car.h file
+            cout << "Lane " << n << endl;
+            if (percent < 50){ //changed to 50%
+                Cque[i].push_back(Car());
+                cout << "Car just joined: " ;
+                Cque[i].back().print();
             }
+            else {
+                cout << "Just paid and left: "; //I added this before popping because after we pop, there will be a new car at the front 
+                Cque[i].front().print();
+                Cque[i].pop_front();
+            }
+            n++;
         }
-        n++;
+        for(int i =0; i < lane; i++){
+            if (percent < leave){ //the paid and joined loops are pretty similar to the onees above
+                cout << "Paid: ";
+                Cque[i].front().print();
+                cout << endl;
+                Cque[i].pop_front();
+            }
+            else if (percent < leave + join){
+                Cque[i].push_back(Car());
+                cout << "Joined: ";
+                Cque[i].back().print();
+            }
+            else if (percent < leave + join + shift){
+                Car temp = Cque[i].back(); //what i did here was I stored one data into temp and then remove it from the initial lanne, and then add it into a new lane
+                Cque[i].pop_back();
+                int Lane;
+                Lane = rand()%lane;
+                Cque[Lane].push_back(temp);
+                cout << "Switched: ";
+                Cque[Lane].back().print();
 
-    }
+            }
+                    if(Cque[i].empty()){
+                cout << "Queue now empty." << endl; //if the que is empty after going through if else statements, this will be printed
+            }
+            else {
+                cout << "Queue: " << endl;
+                for (auto&c : Cque[i]){
+                c.print(); // the print() comed from Car.h file
+                }
+        }
+        }
 }
 /*The three possible operations and their probabilities are:
 
